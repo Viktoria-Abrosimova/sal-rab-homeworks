d273b598996f78f09bba1de70f12d9f3c74ec63d
Viktoria Abrosimova <abrosimova.vd@gmail.ru>
2022-04-21 19:30:00 +0300

    dz5

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,14 +1,24 @@
-function sendRequest(name, phone, address, sum,goods) {
-  let data  = {client: {name, phone},order:{address, sum}, goods:[count,title]};
-    let countOfGoods = goods.length;
-    for (let i = 0; i <= countOfGoods; i ++) {
-        data.goods.push(goods[i].title);
-        data.goods.push(goods[i].count);
-    }
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-    data.client = 'Иван';
-    let jsonData = JSON.stringify(data);
-    return jsonData;
+function sendRequest(name, phone, address, goods, sum) {
+      
+  let data = {goods: [], order: {}};
+
+  data.client = name + " " + phone;   
+  
+  let addr = "ул. " + address.street +", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
+  let order = {};
+  order.address = addr; order.sum = sum;
+  data.order = order;
+   
+   let countOfGoods = goods.length;
+   for (let i = 0; i < countOfGoods; i += 1) {
+       let title = goods[i].title;
+       let count = goods[i].count;
+       let    good = {title, count};
+       data.goods.push(good);
+   }
+
+   let jsonData = JSON.stringify({data: data});
+
+   return jsonData;
 }
  
